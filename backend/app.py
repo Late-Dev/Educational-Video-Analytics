6e7d6c288a33ce51c4adafd7c6dfdce96a7490ad
Vladimir Fomenko <vlakoresker@gmail.com>
2022-06-25 08:44:48 +0500

    add get_video_list

--- backend/app.py
+++ backend/app.py
@@ -1,7 +1,7 @@
 from fastapi import FastAPI
 from fastapi.encoders import jsonable_encoder
 
-from database import add_video_data
+from database import add_video_data, get_video_list_data
 from models import VideoSchema
 
 app = FastAPI()
@@ -16,3 +16,9 @@ async def add_video(video: VideoSchema):
     video = jsonable_encoder(video)
     await add_video_data(video)
     return "Success"
+
+
+@app.get("/get_video_list")
+async def get_video_list():
+    result = await get_video_list_data()
+    return result
