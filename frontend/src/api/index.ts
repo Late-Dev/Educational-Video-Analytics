cff95dbbea190c9aefa5926e46290217254f76ff
Ivan Ryazanov <i.riazanov@napoleonit.ru>
2022-06-26 01:31:52 +0600

    added env files

--- frontend/src/api/index.ts
+++ frontend/src/api/index.ts
@@ -1,8 +1,8 @@
 import axios from "axios";
 
 
-const API_URL = "http://localhost:8000";
-
+// const API_URL = "http://localhost:8000";
+const API_URL = process.env.API_URL
 axios.defaults.baseURL = API_URL
 
 export interface Video{
