82a5b5e851491ae43a8e72a8413748b13ec88ad9
Ivan Ryazanov <i.riazanov@napoleonit.ru>
2022-06-26 00:35:25 +0600

    shit rempved

--- backend/app.py
+++ backend/app.py
@@ -11,9 +11,9 @@ origins = [
     "http://localhost",
     "http://localhost:8080",
     "http://localhost:8081",
-    "http://http://127.0.0.1:8080"
-    "http://http://127.0.0.1:8081"
-    "http://http://127.0.0.1"
+    "http://127.0.0.1:8080",
+    "http://127.0.0.1:8081",
+    "http://127.0.0.1",
 ]
 
 app.add_middleware(
